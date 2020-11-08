309769ddd04663acc8f92b87063c393d75a2c363
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2020-11-08 17:55:19 -0300

    task: delete users

--- src/modules/user/infra/http/controllers/UserController.ts
+++ src/modules/user/infra/http/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import CreateUserService from '@modules/user/services/CreateUserService';
 import FindUsersService from '@modules/user/services/FindUsersService';
 import FindUserService from '@modules/user/services/FindUsersService';
+import RemoveUserService from '@modules/user/services/RemoveUserService';
 import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 
@@ -35,20 +36,20 @@ export default class UserController {
 
     const createUser = container.resolve(CreateUserService);
 
-    return await createUser.execute({
-      username,
-      full_name,
-      age,
-      password,
-      email,
-      UF,
-      city,
-      cpf,
-      isInfluencer,
-    })
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(403).json(err));
-
+    return await createUser
+      .execute({
+        username,
+        full_name,
+        age,
+        password,
+        email,
+        UF,
+        city,
+        cpf,
+        isInfluencer,
+      })
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(403).json(err));
   }
 
   public async index(req: Request, res: Response): Promise<Response> {
@@ -63,7 +64,7 @@ export default class UserController {
     const { id } = req.params;
 
     if (!id) {
-      return res.status(401).json({ message: "parameter not found" });
+      return res.status(401).json({ message: 'parameter not found' });
     }
 
     const findedUser = container.resolve(FindUserService);
@@ -71,5 +72,19 @@ export default class UserController {
     const findUser = await findedUser.execute();
 
     return res.status(200).json(classToClass(findUser));
+  }
+
+  public async remove(req: Request, res: Response): Promise<Response> {
+    const { id } = req.body;
+
+    if (!id) {
+      return res.status(401).json({ message: 'parameter not found' });
+    }
+
+    const removeUser = container.resolve(RemoveUserService);
+
+    const removedUser = await removeUser.execute(id);
+
+    return res.status(200).json(classToClass(removedUser));
   }
 }
--- src/modules/user/infra/http/routes/users.routes.ts
+++ src/modules/user/infra/http/routes/users.routes.ts
@@ -8,5 +8,6 @@ const usersRouter = Router();
 usersRouter.post('/', UserController.create);
 usersRouter.get('/', UserController.index);
 usersRouter.get('/:id', UserController.show);
+usersRouter.delete('/', UserController.remove);
 
 export default usersRouter;
