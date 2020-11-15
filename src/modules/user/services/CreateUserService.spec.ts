4d65847049a7018cdc6eadb519b87ab094e86227
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2020-11-15 17:18:28 -0300

    test: Should return 400 if an email already exists

--- src/modules/user/services/CreateUserService.spec.ts
+++ src/modules/user/services/CreateUserService.spec.ts
@@ -1,3 +1,34 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import CreateUserService from './CreateUserService';
+import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
+import AppError from '@shared/errors/AppError';
+
+describe('CreateUser', () => {
+  test('Should return 400 if email already exists ', async () => {
+    const fakeUser = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUser);
+
+    await createUser.execute({
+      UF: 'SP',
+      age: 11,
+      city: 'Guarujá',
+      email: 'Pedro@mail.com',
+      full_name: 'Pedro Emanoel do Nascimento',
+      isInfluencer: false,
+      password: '12345',
+      username: 'Renato Gaucho',
+    });
+
+    expect(
+      createUser.execute({
+        UF: 'SP',
+        age: 11,
+        city: 'Guarujá',
+        email: 'Pedro@mail.com',
+        full_name: 'Pedro Emanoel do Nascimento',
+        isInfluencer: false,
+        password: '12345',
+        username: 'Renato Gaucho',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  });
 });
