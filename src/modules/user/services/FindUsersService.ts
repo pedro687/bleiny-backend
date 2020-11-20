64d1f586839bc8c72b2709cd3c94a73a96aeb12f
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2020-11-20 19:42:17 -0300

    feat: show all users

--- src/modules/user/services/FindUsersService.ts
+++ src/modules/user/services/FindUsersService.ts
@@ -1,8 +1,10 @@
+import "reflect-metadata";
 import { container, inject, injectable } from 'tsyringe';
 import Users from '@modules/user/infra/typeorm/entities/Users';
 
 import IUserRepository from '@modules/user/repositories/IUserRepository';
 import AppError from '@shared/errors/AppError';
+import IFindAllUsersDTO from '../DTOs/IFindAllUsers';
 
 @injectable()
 export default class FindUsersService {
@@ -16,8 +18,8 @@ export default class FindUsersService {
     this.usersRepository = usersRepository;
   }
 
-  public async execute(user_id: string): Promise<Array<Users>> {
-    const findUsers = await this.usersRepository.findAll({except_user_id: user_id});
+  public async execute({ except_user_id }: IFindAllUsersDTO): Promise<Array<Users>> {
+    const findUsers = await this.usersRepository.findAll({except_user_id: except_user_id});
 
     if (!findUsers) {
       throw new AppError('No users found', 401);
